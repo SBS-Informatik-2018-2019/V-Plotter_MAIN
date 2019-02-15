36dcb1cd233390711e832fa10e4a6daace23ffb4
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-02-15 19:12:20 +0100

    Update parser.py

--- code/parser.py
+++ code/parser.py
@@ -9,6 +9,20 @@ punkteX = [0,0] # do not edit points
 punkteY = [0,0] # do not edit points
 #END VARIABLES####################################
 
+# oder mit liste
+liste = list(range(3,7)) # [3,4,5,6]
+liste.append(8)          # [3,4,5,6,8]
+liste.insert(4,7)        # [3,4,5,6,7,8]
+liste.extend([2,4,6])    # [3,4,5,6,7,8,2,4,6]
+print(liste.count(6))    # 2; 2 mal ist '6' in liste
+liste.remove(6)          # [3,4,5,7,8,2,4,6]
+print(liste.count(6))    # 1; 1 mal ist '6' in liste
+print(liste.index(4))    # 1; erste '4' ist bei Index 1
+liste.sort()             # [2,3,4,4,5,6,7,8]
+liste.reverse()          # [8,7,6,5,4,4,3,2]
+print(liste.pop())       # 2; [8,7,6,5,4,4,3]
+#
+
 def svgLesen():
     svgRootElement = ET.parse(FILE).getroot()
     polylineElement = svgRootElement.find("{http://www.w3.org/2000/svg}path")

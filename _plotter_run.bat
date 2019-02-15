9391541e44e1ed9c3144132d60f25aed13882774
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-02-15 18:55:28 +0100

    Update _plotter_run.bat

--- _plotter_run.bat
+++ _plotter_run.bat
@@ -1,2 +1,2 @@
-psftp.exe -b plotter_trans_cmd.txt -pw 123456789 pi@192.168.178.210
-plink.exe -m plotter_run_cmd.txt -pw 123456789 pi@192.168.178.210
+psftp.exe -b plotter_trans_cmd.txt -pw raspberry pi@10.3.12.46
+plink.exe -m plotter_run_cmd.txt -pw raspberry pi@10.3.12.46

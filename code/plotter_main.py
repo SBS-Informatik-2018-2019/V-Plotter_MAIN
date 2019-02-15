a84321361ac52caa85dcbfad391dc4bcc7de755a
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-02-15 19:08:54 +0100

    Update plotter_main.py

--- code/plotter_main.py
+++ code/plotter_main.py
@@ -1,1 +1,10 @@
-print("-V---P-l-o-t-t-e-r-----------------------------------------------")
+#-*- coding: utf-8 -*-
+from time import sleep
+
+if __name__ == "__main__":
+    print("-V---P-l-o-t-t-e-r-----------------------------------------------")
+    sleep(1)
+    print("Bitte den Anweisungen hier folgen!")
+    sleep(1)
+    print("-Viel Spass...")
+    pass

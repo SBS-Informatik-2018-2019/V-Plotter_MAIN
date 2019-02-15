99ced028b19296d101610517feb1a46b3e363150
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-02-15 18:50:01 +0100

    added file transfer and python autostart

---
+++ _plotter_run.bat
@@ -0,0 +1,2 @@
+psftp.exe -b plotter_trans_cmd.txt -pw 123456789 pi@192.168.178.210
+plink.exe -m plotter_run_cmd.txt -pw 123456789 pi@192.168.178.210
--- plotter_run.bat
+++
@@ -1,3 +0,0 @@
-
-plink.exe -C -pw 123456789 pi@192.168.178.210 <cmd.txt
-echo Hallo

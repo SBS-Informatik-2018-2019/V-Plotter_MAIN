b5c59947789a6975b90af53b2f83ef9fa2574ed4
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-02-16 13:20:14 +0100

    .

--- _plotter_run.bat
+++ _plotter_run.bat
@@ -1,2 +1,19 @@
-psftp.exe -b plotter_trans_cmd.txt -pw raspberry pi@10.3.12.46
-plink.exe -m plotter_run_cmd.txt -pw raspberry pi@10.3.12.46
+@echo off
+cls
+echo -V-Plotter------------------------
+echo -Mit dem Raspberry verbinden...
+echo -Verbindung testen
+timeout /T 2
+ping /n 1 192.168.178.210
+timeout /T 2
+cls
+echo -Datei "file.svg" wird uebertragen
+timeout /T 2
+psftp.exe -b plotter_trans_cmd.txt -pw raspberry pi@192.168.178.210
+timeout /T 2
+cls
+echo -Plotter wird gestartet
+plink.exe -m plotter_run_cmd.txt -pw raspberry pi@192.168.178.210
+echo -Plotter beendet
+pause
+exit

542f03b14c6e78dba737168e50c47581c07dbdde
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-02-17 11:57:47 +0100

    Update plotter_main.py

--- parserTest/plotter_main.py
+++ parserTest/plotter_main.py
@@ -1,7 +1,6 @@
 #-*- coding: utf-8 -*-
 from time import sleep
 import svg_parser
-import PyV8
 
 if __name__ == "__main__":
     print("auf Antwort warten...")
@@ -9,27 +8,4 @@ if __name__ == "__main__":
     sleep(1)
     print("Bitte den Anweisungen hier folgen!")
     print("-> mit [Strg] + [C] beenden")
-    sleep(2)
-    print("-Viel Spass...")
-    
-    ctx = PyV8.JSContext()
-    ctx.enter()
-
-    js = """
-    function escramble_758(){
-    var a,b,c
-    a='+1 '
-    b='84-'
-    a+='425-'
-    b+='7450'
-    c='9'
-    document.write(a+c+b)
-    }
-    escramble_758()
-    """
-
-    print ctx.eval(js.replace("document.write", "return "))
-    
-    svg_parser.parse("file.svg")
-    
     

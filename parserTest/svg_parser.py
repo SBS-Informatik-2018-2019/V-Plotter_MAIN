090c796fd48f8d377993bf8ec68416a8ca4ec3d6
Patrick <Bug-Patrick@users.noreply.github.com>
2019-02-18 00:14:33 +0100

    Parsing bis zum Tode
    Ich glaube eine kleine Erleuchtung hatte ich noch um Mitternacht, morgen hoffentlich mehr.

--- parserTest/svg_parser.py
+++ parserTest/svg_parser.py
@@ -14,6 +14,10 @@ groesseY = 10000
 nameSpace={'svgNs':'http://www.w3.org/2000/svg'}
 #END VARIABLES####################################
 
+def parse(file):
+    svgLesen()
+    return
+
 # oder mit liste
 liste = list(range(3,7)) # [3,4,5,6]
 liste.append(8)          # [3,4,5,6,8]
@@ -28,7 +32,7 @@ liste.reverse()          # [8,7,6,5,4,4,3,2]
 print(liste.pop())       # 2; [8,7,6,5,4,4,3]
 #
 
-# by Niklas Start des Einlesens einer svg-Datei
+# Start des Einlesens einer svg-Datei
 def svgLesen():
     svgRootElement = ET.parse(FILE).getroot()
     if (svgRootElement.find("svgNs:path", nameSpace) != None):
@@ -43,57 +47,83 @@ def svgLesen():
         machePolyline(points)
     return
 
-# by Paul erstellen einer linearen Liste mit allen Punkten
-def punkteSchreiben():
-    global punkteX
-    global punkteY
-
-    scalieren()
+# Erstellen einer linearen Liste aus den Punkten von Geraden
+def punkteSchreiben(points):
+    # Schleife über die gesamte Länge des Strings points
+    while len(points) != 0:
+        # separate x coord from points
+        points = points.lstrip()
+        index = points.index(',')
+        x = float(points[0:index])
+        if(x<=0):
+            x = 1
+        # separate y coord from points
+        points = points[index+1:len(points)]
+        points = points.lstrip()
+        index = points.index(' ')
+        y = float(points[0:index])
+        if(y<=0):
+            y = 1
+        
+        points = points[index+1:len(points)]
+        points = points.lstrip()
+#        if(LT.macheGerade(x2, y2)=="stop"):
+#            return "stop"
+        # Vormerken der Position des Plotters?
+        continue
+    skalieren()
     return
 
-#by Paul Skalieren der Größe auf eine vorbestimmtes Feld des Plotters
-def scalieren():
+def convertCurve(curveCoord):
+    pass
+
+# Skalieren der Größe auf eine vorbestimmtes Feld des Plotters
+def skalieren():
     return
-
-
 
 def machePath(path_elements):
+    #Startposition
+    index = indexNaechsterBuchstabe(path_elements[1: len(path_elements)])
+    pos = path_elements[1: index]
+    index = path_elements.index(',')
+    x1 = pos[0: index-1]
+    y1 = pos[index: len(pos)]
+    print("StartX: " + x1 + " StartY: " + y1)
 
     while len(path_elements) != 0:
         if(path_elements[0] == "M"):
             print("MOVETO - Bitte entfernen sie den Stift!")
-            path_elements = path_elements[1: len(path_elements)-1]
+            path_elements = path_elements[1: len(path_elements)]
 #           plotterio.aufOKwarten()
             path_elements = path_elements.lstrip()
             index = indexNaechsterBuchstabe(path_elements)
             print(path_elements[0: index])
+            path_elements = path_elements[index: len(path_elements)]
 
         elif(path_elements[0] == "L"):
             print("LINETO - Zeichnen einer Geraden")
-            path_elements = path_elements[1: len(path_elements)-1]
+            path_elements = path_elements[1: len(path_elements)]
 #           plotterio.aufOKwarten()
             path_elements = path_elements.lstrip()
             index = indexNaechsterBuchstabe(path_elements)
             print(path_elements[0: index])
+            path_elements = path_elements[index: len(path_elements)]
 
-#        path_elements.find(0)
-#        path_elements.index("M")
 
 def indexNaechsterBuchstabe(string):
     index = len(string)
-    if index > string.find('M', 0, index):
+    if index > string.find('M', 0, index) and string.find('M', 0, index) > 0:
         index = string.find('M', 0, index)
-    if index > string.find('L', 0, index):
+    if index > string.find('L', 0, index) and string.find('L', 0, index) > 0:
         index = string.find('L', 0, index)
-    elif index > string.find('C', 0, index):
+    elif index > string.find('C', 0, index) and string.find('C', 0, index) > 0:
         index = string.find('C', 0, index)
-    elif index > string.find('Z', 0, index):
+    elif index > string.find('Z', 0, index) and string.find('Z', 0, index) > 0:
         index = string.find('Z', 0, index)
     return index
 
-# by Paul schrittweise Ansteuern der Koordinaten eine Polyline
+# schrittweise Ansteuern der Koordinaten eine Polyline
 def machePolyline(points):
-
     # separate x1 coord from points
     points = points + " "
     points = points.lstrip()
@@ -135,8 +165,4 @@ def machePolyline(points):
         x1 = x2
         y1 = y2
         continue
-    return
-
-def parse(file):
-    svgLesen()
     return

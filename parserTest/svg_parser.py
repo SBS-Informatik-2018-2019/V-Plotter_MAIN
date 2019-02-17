b36b0e4275b1257dded507a30e2ddf7ab5a52e8f
Patrick <Bug-Patrick@users.noreply.github.com>
2019-02-17 11:29:14 +0100

    Parser tests

--- parserTest/svg_parser.py
+++ parserTest/svg_parser.py
@@ -32,6 +32,7 @@ def svgLesen():
         pathElement = svgRootElement.find("svgNs:path", nameSpace)
         string = pathElement.get('d')
         print("-> PATH: " + string)
+        machePath()
     elif (svgRootElement.find("svgNs:polyline", nameSpace) != None):
         polylineElement = svgRootElement.find("svgNs:polyline", nameSpace)
         points = polylineElement.get('points')
@@ -67,7 +68,7 @@ def machePolyline(points):
     points = points[index+1:len(points)]
     points = points.lstrip()
 
-    #LT.initPosition(STARTX,STARTY) Paul? STARTX= x1; Startposition setzen, wie bei MOVETO in path
+    #LT.initPosition(STARTX,STARTY) Paul Startkoordinaten an Motor übergeben
 
     # Schleife über die gesamte Länge des Strings points (Nutzen einer Liste?)
     while len(points) != 0:

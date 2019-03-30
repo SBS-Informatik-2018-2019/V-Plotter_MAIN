90290c74a283a8aa374139bbb9caa1d1156cd62e
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-03-30 16:01:57 +0100

    .paul
    new parsing process

---
+++ code/filereader.py
@@ -0,0 +1,31 @@
+#-*- coding: utf-8 -*-
+import xml.etree.ElementTree as ET
+import os.path as OSPATH
+import sys as SYS
+import tokenizer as TOKER
+
+#VARIABLES#######################################
+SVG_FILE = "file.svg"
+nameSpace={'svgNs':'http://www.w3.org/2000/svg'}
+#END VARIABLES####################################
+
+# Start des Einlesens einer svg-Datei
+def svgLesen():
+    if(OSPATH.exists(SVG_FILE)):
+        svgRootElement = ET.parse(SVG_FILE).getroot()
+        # Einlesen eines Path
+        if (svgRootElement.find("svgNs:path", nameSpace) != None):
+            pathElement = svgRootElement.find("svgNs:path", nameSpace)
+            path = pathElement.get('d')
+            TOKER.tokenize_path(path)
+
+        # Anders wird eine Polyline eingelesen
+        elif (svgRootElement.find("svgNs:polyline", nameSpace) != None):
+            polylineElement = svgRootElement.find("svgNs:polyline", nameSpace)
+            points = polylineElement.get('points')
+            TOKER.tokenize_polyline(points)
+        else:
+            SYS.exit("[WARN]: in File:" + SVG_FILE + " wurde kein Path oder Polyline gefunden!")
+
+    else:
+        SYS.exit("[WARN]: File:" + SVG_FILE + " konnte nicht gefunden werden!")
--- code/main.py
+++ code/main.py
@@ -1,19 +1,21 @@
 #-*- coding: utf-8 -*-
 from time import sleep
-import svg_parser
-import scaler as SCALER
+import filereader as FILER
 
 if __name__ == "__main__":
-    SCALER.initScale()
-    while(True):
-        print("auf Antwort warten...")
-        print("-V---P-l-o-t-t-e-r-----------------------------------------------")
-        sleep(1)
-        print("Bitte den Anweisungen hier folgen!")
-        print("-> mit [Strg] + [C] beenden")
-        sleep(2)
-        print("-Viel Spass...")
-        svg_parser.initParser()
-        svg_parser.parse_standart()
-        print("Druck fertig")
-        sleep(3)
+    print("[SYS]: Warten...")
+    print("[INFO]: Bitte den Anweisungen folgen!")
+    print("[INFO]: mit [Strg] + [C] beenden")
+    sleep(2)
+    print("[SYS]: File lesen...")
+    FILER.svgLesen()
+    sleep(2)
+    print("[SYS]: fertig")
+    print("[SYS]: Motorbewegungen vorbereiten...")
+    #TODO
+    sleep(2)
+    print("[SYS]: fertig")
+    print("[SYS]: Drucken...")
+    #TODO
+    print("[SYS]: fertig")
+    sleep(3)

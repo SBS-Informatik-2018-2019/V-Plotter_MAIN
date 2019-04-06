550e0a62ae745216688af9d5bbd817b8c5f9c09f
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-04-06 20:24:06 +0200

    add informatik.svg

---
+++ tools/path_to_points/script.js
@@ -0,0 +1,26 @@
+
+var numPoints = 300;
+
+
+var numbernode = document.getElementById("numbernode");
+numbernode.innerHTML = numPoints;
+
+var mypath = document.getElementById("mypath");
+var pathLength = mypath.getTotalLength();
+var polinePoints = [];
+
+for (var i = 0; i < numPoints; i++) {
+    var p = mypath.getPointAtLength(i * pathLength / numPoints);
+    polinePoints.push(p.x);
+    polinePoints.push(",");
+    polinePoints.push(p.y);
+    polinePoints.push("\n");
+}
+
+var mypolygon = document.getElementById("mypolyline");
+mypolygon.setAttribute("points", polinePoints.join(" "));
+var mytextarea = document.getElementById("mytextarea");
+polinePoints.unshift("<polyline points='\n");
+polinePoints.push("' />");
+
+mytextarea.innerHTML = polinePoints.join(" ");

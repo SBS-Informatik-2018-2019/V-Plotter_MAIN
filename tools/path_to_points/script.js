ff494ecbdd87f99b3e7dc4c0006992b9c7c40751
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-04-07 14:59:31 +0200

    new hml js css

--- tools/path_to_points/script.js
+++ tools/path_to_points/script.js
@@ -1,26 +1,66 @@
+function run() {
 
-var numPoints = 350;
+    // selected numbers
+    var numPointsPolyline = document.getElementById("numPointsPolyline").value;
+    var numPointsPathSegment = document.getElementById("numPointsPathSegment").value;
 
 
-var numbernode = document.getElementById("numbernode");
-numbernode.innerHTML = numPoints;
+    // put path in list
+    var mytextareapathold = document.getElementById("mytextareapathold");
+    document.getElementById("mypath").setAttribute("d", mytextareapathold.value);
 
-var mypath = document.getElementById("mypath");
-var pathLength = mypath.getTotalLength();
-var polinePoints = [];
+    // one polyline from the path
+    var mypath = document.getElementById("mypath");
+    var pathLength = mypath.getTotalLength();
+    var polinePoints = [];
+    for (var i = 0; i < numPointsPolyline; i++) {
+        var p = mypath.getPointAtLength(i * pathLength / numPointsPolyline);
+        polinePoints.push(p.x);
+        polinePoints.push(",");
+        polinePoints.push(p.y);
+        polinePoints.push("\n");
+    }
+    var mypolyline = document.getElementById("mypolyline");
+    mypolyline.setAttribute("points", polinePoints.join(" "));
+    polinePoints.unshift("<polyline points='\n");
+    polinePoints.push("' />");
+    var mytextareapolyline = document.getElementById("mytextareapolyline");
+    mytextareapolyline.innerHTML = polinePoints.join(" ");
 
-for (var i = 0; i < numPoints; i++) {
-    var p = mypath.getPointAtLength(i * pathLength / numPoints);
-    polinePoints.push(p.x);
-    polinePoints.push(",");
-    polinePoints.push(p.y);
-    polinePoints.push("\n");
+    // path only with lines
+    var mypath = document.getElementById("mypath");
+    var pathdata = mypath.getAttribute("d").toUpperCase();
+    var paths = [];
+    paths = pathdata.split("M");
+    var newpath = [];
+    console.log(paths);
+    for (var j = 1; j < paths.length; j++) {
+        var element = paths[j];
+        var pathdummy = document.getElementById("dummy");
+        pathdummy.setAttribute("d", "M " + element);
+        var pathDummyLength = pathdummy.getTotalLength();
+        console.log(pathdummy.getAttribute("d"));
+        var p = pathdummy.getPointAtLength(0.0000000001);
+        newpath.push("M");
+        newpath.push(p.x);
+        newpath.push(p.y);
+        newpath.push("L");
+        for (var i = 1; i < numPointsPathSegment; i++) {
+            var p = pathdummy.getPointAtLength(i * pathDummyLength / numPointsPathSegment);
+            newpath.push(p.x);
+            newpath.push(p.y);
+        }
+        
+    }
+    document.getElementById("dummy").removeAttribute("d")
+    var newpathelement = document.getElementById("new");
+    newpathelement.setAttribute("d", newpath.join(" "))
+
+    newpath.unshift("<path d='")
+    var mytextareapathnew = document.getElementById("mytextareapathnew");
+    newpath.push("' />")
+    mytextareapathnew.innerHTML = newpath.join(" ");
+
+
+    
 }
-
-var mypolygon = document.getElementById("mypolyline");
-mypolygon.setAttribute("points", polinePoints.join(" "));
-var mytextarea = document.getElementById("mytextarea");
-polinePoints.unshift("<polyline points='\n");
-polinePoints.push("' />");
-
-mytextarea.innerHTML = polinePoints.join(" ");
